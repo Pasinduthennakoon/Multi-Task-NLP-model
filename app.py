eb65a7a78a3326e596ec178d470b77dfc46fcb86
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-03-05 12:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from prediction_pipeline import predict_content
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+class PredictionRequest(BaseModel):
+    text: str
+
 @app.post('/predict')
-def predict_result(text):
-    return predict_content(text)
+def predict_result(text : PredictionRequest):
+    return predict_content(text.text)
