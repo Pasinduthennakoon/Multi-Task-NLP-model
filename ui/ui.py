12ee14de1615acf2804f5b52f1817f70f02280aa
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2026-03-06 11:43:43 +0530

    update location of ui.py

--- ui/ui.py
+++ ui/ui.py
@@ -1,12 +1,15 @@
+import os
 import streamlit as st
 import requests
+
+API_URL = os.getenv("API_URL")
 
 st.title("Content Safety Classifier")
 user_input = st.text_area("Enter text to analyze:")
 
 if st.button("Predict"):
     if user_input:
-        response = requests.post("http://127.0.0.1:8000/predict", json={"text": user_input})
+        response = requests.post(f"{API_URL}/predict", json={"text": user_input})
         
         if response.status_code == 200:
             res = response.json()
